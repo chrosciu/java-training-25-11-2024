838708b07a9bab003f3d5a2f5044ef4a96a13881
Marcin Chrost <mchrost82@gmail.com>
2024-11-26 23:02:40 +0100

    AreaMeanCalculator - solution

--- src/eu/chrost/javatraining/day2/exercises/methods/ArrayMeanCalculator.java
+++ src/eu/chrost/javatraining/day2/exercises/methods/ArrayMeanCalculator.java
@@ -11,4 +11,35 @@ o	average(int[][] matrix) – oblicza średnią wszystkich elementów w dwuwymia
  */
 
 public class ArrayMeanCalculator {
+    public static void main(String[] args) {
+        int[] numbers = {2, 5, 7};
+        System.out.println(average(numbers));
+
+        int[][] matrix = {
+                {3, 4},
+                {1, 3, 6}
+        };
+        System.out.println(average(matrix));
+    }
+
+    public static double average(int[] numbers) {
+        double sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.length;
+    }
+
+    public static double average(int[][] matrix) {
+        double sum = 0;
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j];
+                ++count;
+            }
+        }
+        return (sum / count);
+    }
+
 }
