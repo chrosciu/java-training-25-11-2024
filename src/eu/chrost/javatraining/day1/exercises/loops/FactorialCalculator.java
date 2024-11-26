e944b94bb429392f4f0bec96c533e43cd74407dd
Marcin Chrost <mchrost82@gmail.com>
2024-11-26 09:41:37 +0100

    Added FactorialCalculator exercise (blank)

---
+++ src/eu/chrost/javatraining/day1/exercises/loops/FactorialCalculator.java
@@ -0,0 +1,13 @@
+package eu.chrost.javatraining.day1.exercises.loops;
+
+/*
+Zadanie 6: Obliczanie silni
+1.	Wczytaj z konsoli liczbę całkowitą N.
+2.	Oblicz silnię liczby N (np. 5! = 5 x 4 x 3 x 2 x 1 = 120) przy użyciu pętli for lub while.
+3.	Wyświetl wynik w formacie:
+
+Silnia liczby 5 wynosi: 120
+ */
+
+public class FactorialCalculator {
+}
