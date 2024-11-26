f74247c21b255809eb14d7d880c4c4c546f476ec
Marcin Chrost <mchrost82@gmail.com>
2024-11-26 11:35:53 +0100

    Added MaxArgument exercise

---
+++ src/eu/chrost/javatraining/day2/exercises/arrays/MaxArgument.java
@@ -0,0 +1,14 @@
+package eu.chrost.javatraining.day2.exercises.arrays;
+
+/*
+
+Zadanie 2: Szukanie największego elementu
+1.	Wczytaj od użytkownika liczb całkowite jak argumenty programu
+2.	Znajdź największy element w tablicy, używając pętli for-each.
+3.	Wyświetl wynik w formacie:
+
+Największy argument to: 42
+ */
+
+public class MaxArgument {
+}
