ee782b67e5871d19a613d7c90a1e158fa1a84f95
Marcin Chrost <mchrost82@gmail.com>
2024-11-27 10:07:50 +0100

    Rectangle class - exercise

---
+++ src/eu/chrost/javatraining/day3/exercises/classes/Rectangle.java
@@ -0,0 +1,18 @@
+package eu.chrost.javatraining.day3.exercises.classes;
+
+/*
+
+Zadanie 2: Klasa Rectangle
+1.	Utwórz klasę Rectangle z polami:
+o	double length
+o	double width
+2.	Dodaj metody:
+o	double calculateArea() – zwraca pole prostokąta.
+o	double calculatePerimeter() – zwraca obwód prostokąta.
+3.	W main():
+o	Utwórz kilka obiektów klasy Rectangle, ustawiając wartości pól bezpośrednio.
+o	Wyświetl pole i obwód prostokątów.
+ */
+
+public class Rectangle {
+}
