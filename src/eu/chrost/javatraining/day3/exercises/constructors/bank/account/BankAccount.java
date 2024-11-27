fc45349c1668fd97e03bb545b348c8621ec57406
Marcin Chrost <mchrost82@gmail.com>
2024-11-27 15:06:32 +0100

    BankAccount + Main - solution

--- src/eu/chrost/javatraining/day3/exercises/constructors/bank/account/BankAccount.java
+++ src/eu/chrost/javatraining/day3/exercises/constructors/bank/account/BankAccount.java
@@ -18,5 +18,47 @@ o	Wyświetla saldo po każdej operacji.
 
  */
 
+import java.math.BigDecimal;
+
 public class BankAccount {
+    private final String accountNumber;
+    private BigDecimal balance;
+
+    public BankAccount(String accountNumber, BigDecimal balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public BankAccount(String accountNumber) {
+        this(accountNumber, BigDecimal.ZERO);
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return this.balance;
+    }
+
+    public void deposit(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) > 0) {
+            this.balance = this.balance.add(amount);
+        }
+    }
+
+    public void withdraw(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) > 0 && this.balance.compareTo(amount) >= 0) {
+            this.balance = this.balance.subtract(amount);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
+
