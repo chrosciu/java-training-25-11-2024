3687becb5cbd6a4f7254ff11ba206254f9fba9c4
Marcin Chrost <mchrost82@gmail.com>
2024-11-27 12:01:34 +0100

    Car class - solution

--- src/eu/chrost/javatraining/day3/exercises/classes/Car.java
+++ src/eu/chrost/javatraining/day3/exercises/classes/Car.java
@@ -20,5 +20,58 @@ o   Wypisz zawartosc obiektu na konsole
 
  */
 
+import java.util.Objects;
+
 public class Car {
+    public String brand;
+    public String model;
+    public int year;
+
+    public boolean isOlderThan(int year) {
+        return this.year > year;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Car car) {
+            return Objects.equals(car.brand, this.brand)
+                    && Objects.equals(car.model, this.model)
+                    && car.year == this.year;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.brand = "Opel";
+        c1.model = "Meriva";
+        c1.year = 2012;
+
+        Car c2 = new Car();
+        c2.brand = "Ford";
+        c2.model = "Fiesta";
+        c2.year = 2005;
+
+        Car c3 = new Car();
+        c3.brand = new String("Opel");
+        c3.model = "Meriva";
+        c3.year = 2012;
+
+        System.out.println("Czy c1 jest starszy od 2000: " + c1.isOlderThan(2000));
+        System.out.println("Czy c1 jest starszy od 2024: " + c1.isOlderThan(2024));
+        System.out.println("Czy c1 to jest c2: " + c1.equals(c2));
+        System.out.println("Czy c1 to jest c3: " + c1.equals(c3));
+        System.out.println(c1);
+
+
+    }
 }
