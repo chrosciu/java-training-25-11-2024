6be29e878bb61bc6cccf0f4cd0cbb6357046904c
Marcin Chrost <mchrost82@gmail.com>
2024-11-27 10:20:28 +0100

    Rectangle class - solution

--- src/eu/chrost/javatraining/day3/exercises/classes/Rectangle.java
+++ src/eu/chrost/javatraining/day3/exercises/classes/Rectangle.java
@@ -15,4 +15,25 @@ o	Wyświetl pole i obwód prostokątów.
  */
 
 public class Rectangle {
+    public double length;
+    public double width;
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double calculatePerimeter() {
+        return length * 2 + width * 2;
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.length = 5;
+        rectangle. width = 10;
+
+        System.out.println("Pole prostokąta wynosi: " + (rectangle.calculateArea()));
+        System.out.println("Obwód prostokąta wynosi: " + (rectangle.calculatePerimeter()));
+
+
+    }
 }
