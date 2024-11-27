92529f76f929980eaa39f99433c75497cc541022
Marcin Chrost <mchrost82@gmail.com>
2024-11-27 09:58:36 +0100

    Person class as exercise

---
+++ src/eu/chrost/javatraining/day3/exercises/classes/Person.java
@@ -0,0 +1,20 @@
+package eu.chrost.javatraining.day3.exercises.classes;
+
+/*
+Zadanie 1: Klasa Person
+1.	Utwórz klasę Person z polami:
+o	String name
+o	int age
+2.	Dodaj metodę instancyjną boolean isAdult(), która zwraca true, jeśli wiek osoby wynosi co najmniej 18 lat.
+3.	W metodzie main():
+o	Utwórz kilka obiektów klasy Person, ustawiając pola bezpośrednio.
+o	Wyświetl wynik metody isAdult() dla różnych osób.
+
+ */
+
+public class Person {
+    public String name;
+    public int age;
+
+    public boolean isAdult()
+}
