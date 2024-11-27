2968f385ea28526d1b8f6684eb5e651d8f26a870
Marcin Chrost <mchrost82@gmail.com>
2024-11-27 15:14:24 +0100

    Rectangle & Student - exercises

---
+++ src/eu/chrost/javatraining/day3/exercises/constructors/shapes/Rectangle.java
@@ -0,0 +1,20 @@
+package eu.chrost.javatraining.day3.exercises.constructors.shapes;
+
+/*
+Zadanie 3: Klasa Rectangle z ograniczonym dostępem
+1.	Utwórz klasę Rectangle z prywatnymi i finalnymi polami:
+o	double length
+o	double width
+2.	Dodaj publiczne metody:
+o	Konstruktor ustawiający wartości pól (muszą być > 0, w przeciwnym razie ustaw domyślnie na 1).
+o   Konstruktor bezparametrowy (ustawia obie wartosci na 1)
+o	Gettery dla obu pól.
+o	Metody double area() i double perimeter(), które zwracają odpowiednio pole i obwód prostokąta.
+3.	W głównym pakiecie utwórz klasę z metodą main(), która:
+o	Tworzy obiekt klasy Rectangle za pomocą konstruktora.
+o   Wypisuje na ekran zawartosc stworzonego prostokata (hint: w klasie Rectangle czegos moze brakowac :))
+o	Wywołuje metody do obliczeń i wyświetla wyniki.
+ */
+
+public class Rectangle {
+}
---
+++ src/eu/chrost/javatraining/day3/exercises/constructors/students/Student.java
@@ -0,0 +1,22 @@
+package eu.chrost.javatraining.day3.exercises.constructors.students;
+
+/*
+
+1.	Utwórz klasę Student z prywatnymi polami:
+o	String name
+o	int id
+o	double gpa
+Na podstawie dalszej tresci zadania, ustal ktore pola powinny byc finalne
+2.	Dodaj publiczne metody:
+o	Konstruktor inicjalizujący wszystkie pola.
+o	Gettery dla wszystkich pól.
+o	void updateGPA(double newGpa) – pozwala zmienić GPA, jeśli newGpa mieści się w przedziale 0–4.
+o	String toString() – zwraca informacje o studencie.
+3.	W głównym pakiecie utwórz klasę z metodą main(), która:
+o	Tworzy kilku studentów i wyświetla ich dane.
+o	Modyfikuje ich GPA, sprawdzając poprawność danych wejściowych.
+
+ */
+
+public class Student {
+}
