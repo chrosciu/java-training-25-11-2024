62c1e0b018f1e70016f46db58495038f9441baed
Marcin Chrost <mchrost82@gmail.com>
2024-11-27 11:37:05 +0100

    Car class - exercise

---
+++ src/eu/chrost/javatraining/day3/exercises/classes/Car.java
@@ -0,0 +1,24 @@
+package eu.chrost.javatraining.day3.exercises.classes;
+
+/*
+
+Zadanie 4: Klasa Car
+1.	Utwórz klasę Car z polami:
+o	String brand
+o	String model
+o	int year
+2.	Dodaj metodę:
+o	boolean isOlderThan(int year) – sprawdza, czy samochód jest starszy niż podany rok.
+3. Nadpisz metode equals() - tak aby porownywala czy dwa samochody maja te same wartosci pol
+4. Nadpisz metode toString() - tak aby zwracala wartosci pol
+5.	W main():
+o	Utwórz kilka obiektów klasy Car, ustawiając wartości pól bezpośrednio.
+o	Wywołaj metodę isOlderThan() dla różnych samochodów.
+o	Porównaj obiekty za pomocą equals() i ==.
+o   Wypisz zawartosc obiektu na konsole
+
+
+ */
+
+public class Car {
+}
