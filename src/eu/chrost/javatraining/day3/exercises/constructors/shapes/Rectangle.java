66c0b46e24e5b4ed7aa3a68da8541844180e30a2
Marcin Chrost <mchrost82@gmail.com>
2024-11-27 15:18:46 +0100

    Rectangle & Student - exercises / update

--- src/eu/chrost/javatraining/day3/exercises/constructors/shapes/Rectangle.java
+++ src/eu/chrost/javatraining/day3/exercises/constructors/shapes/Rectangle.java
@@ -10,7 +10,7 @@ o	Konstruktor ustawiający wartości pól (muszą być > 0, w przeciwnym razie u
 o   Konstruktor bezparametrowy (ustawia obie wartosci na 1)
 o	Gettery dla obu pól.
 o	Metody double area() i double perimeter(), które zwracają odpowiednio pole i obwód prostokąta.
-3.	W głównym pakiecie utwórz klasę z metodą main(), która:
+3.	W klasie Main metode main(), która:
 o	Tworzy obiekt klasy Rectangle za pomocą konstruktora.
 o   Wypisuje na ekran zawartosc stworzonego prostokata (hint: w klasie Rectangle czegos moze brakowac :))
 o	Wywołuje metody do obliczeń i wyświetla wyniki.
--- src/eu/chrost/javatraining/day3/exercises/constructors/students/Student.java
+++ src/eu/chrost/javatraining/day3/exercises/constructors/students/Student.java
@@ -1,7 +1,7 @@
 package eu.chrost.javatraining.day3.exercises.constructors.students;
 
 /*
-
+Zadanie 4: System ocen studenta
 1.	Utwórz klasę Student z prywatnymi polami:
 o	String name
 o	int id
@@ -12,7 +12,7 @@ o	Konstruktor inicjalizujący wszystkie pola.
 o	Gettery dla wszystkich pól.
 o	void updateGPA(double newGpa) – pozwala zmienić GPA, jeśli newGpa mieści się w przedziale 0–4.
 o	String toString() – zwraca informacje o studencie.
-3.	W głównym pakiecie utwórz klasę z metodą main(), która:
+3.	W klasie Main metode main(), która:
 o	Tworzy kilku studentów i wyświetla ich dane.
 o	Modyfikuje ich GPA, sprawdzając poprawność danych wejściowych.
 
