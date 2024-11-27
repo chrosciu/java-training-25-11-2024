9b12794c2844068a6eadf876118fe3367520af35
Marcin Chrost <mchrost82@gmail.com>
2024-11-27 12:05:42 +0100

    Product class - exercise

---
+++ src/eu/chrost/javatraining/day3/exercises/classes/Product.java
@@ -0,0 +1,24 @@
+package eu.chrost.javatraining.day3.exercises.classes;
+
+/*
+
+Zadanie 8: Klasa Product
+1.	Utwórz klasę Product z polami:
+o	String name
+o	double price
+o	int quantity
+2.	Dodaj metody:
+o	double calculateTotalPrice() – oblicza łączną cenę produktu (price * quantity).
+o	boolean isSamePrice(Product other) – sprawdza, czy dwa produkty mają taką samą cenę.
+3. Nadpisz metode equals() - tak aby porownywala czy dwa prodkty maja te same wartosci pol
+4. Nadpisz metode toString() - tak aby zwracala wartosci pol
+5.	W main():
+o	Utwórz kilka obiektów klasy Product, ustawiając wartości pól bezpośrednio.
+o	Porównaj obiekty za pomocą equals() i ==.
+o	Oblicz łączną cenę produktów.
+
+
+ */
+
+public class Product {
+}
