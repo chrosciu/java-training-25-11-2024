74687dac742d0286b1c5379380696bd6f9e6c56c
Marcin Chrost <mchrost82@gmail.com>
2024-11-28 15:22:42 +0100

    Inheritance exercises (vehicles & animals) - amendment

--- src/main/java/eu/chrost/javatraining/day4/exercises/inheritance/vehicles/Main.java
+++ src/main/java/eu/chrost/javatraining/day4/exercises/inheritance/vehicles/Main.java
@@ -7,7 +7,7 @@ Zadanie 2: Polimorfizm w pojazdach
 o	public String getType(), która zwraca tekst "Generic Vehicle".
 2.	Utwórz klasy Car i Truck, które dziedziczą po Vehicle:
 o	Nadpisz metodę getType() w obu klasach, aby zwracała odpowiednio "Car" i "Truck".
-3.	W metodzie main utwórz listę pojazdów (List<Vehicle>) zawierającą różne typy pojazdów.
+3.	W metodzie main utwórz tablice pojazdów (Vehicle[]) zawierającą różne typy pojazdów.
 Wykorzystaj polimorfizm, aby wywołać metodę getType() na każdym obiekcie.
 
  */
