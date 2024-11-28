8878a9331462713e15a643d5bb1385d5081eac33
Marcin Chrost <mchrost82@gmail.com>
2024-11-28 11:49:44 +0100

    StringUtilityDemo extension - exercise

--- src/eu/chrost/javatraining/day4/lectures/libraries/StringUtilityDemo.java
+++ src/eu/chrost/javatraining/day4/lectures/libraries/StringUtilityDemo.java
@@ -4,6 +4,18 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
 
+/*
+
+Zadanie: rozszerzyc dzialanie klasy StringUtilityDemo
+
+- wczytac z konsoli pojedynczy znak
+   hint: wczytuje sie zawsze cala linia, wiec trzeba wziac z niej pierwsdzy znak - charAt()
+   hint: pojedynczy znak jest typu char
+- sprawdzic ile razy wczytany znak wystepuje we wczytanej juz wczesniej i sprawdzonej linii
+   hint: wykorzystac w tym celu metode countMatches() z klasy StringUtils()
+- wyswietlic wynik
+ */
+
 public class StringUtilityDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,5 +29,11 @@ public class StringUtilityDemo {
         }
 
         System.out.println("Line is correct, to be continued...");
+
+        //wczytanie znaku
+
+        //sprawdzenie ile razy znak wystapil w linii
+
+        //wyswietlic wynik
     }
 }
