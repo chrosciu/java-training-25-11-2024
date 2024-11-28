299e3d72313dec6db32539169dfe24865adbc522
Marcin Chrost <mchrost82@gmail.com>
2024-11-28 10:43:04 +0100

    UnitConverter & Config - solution

--- src/eu/chrost/javatraining/day4/exercises/statics/Config.java
+++ src/eu/chrost/javatraining/day4/exercises/statics/Config.java
@@ -18,4 +18,38 @@ o	Pobierz instancję konfiguracji i wyświetl nazwę oraz wersję aplikacji
  */
 
 public class Config {
+    private static final String APP_NAME = "MyApp";
+    private static final String VERSION = "1.0.0";
+
+    private static Config instance;
+
+    public String getAppName() {
+        return APP_NAME;
+    }
+
+    public String getVersion() {
+        return VERSION;
+    }
+
+    public static Config getInstance() {
+        if (instance == null) {
+            instance = new Config();
+        }
+        return instance;
+    }
+
+    private Config() {
+    }
+
+    public static void main(String[] args) {
+        Config config1 = Config.getInstance();
+        System.out.println(config1.getAppName());
+        System.out.println(config1.getVersion());
+
+        Config config2 = Config.getInstance();
+        System.out.println(config2.getAppName());
+        System.out.println(config2.getVersion());
+
+        System.out.println(config1 == config2);
+    }
 }
--- src/eu/chrost/javatraining/day4/exercises/statics/UnitConverter.java
+++ src/eu/chrost/javatraining/day4/exercises/statics/UnitConverter.java
@@ -13,4 +13,20 @@ o	Wykorzystaj metody do przeliczania kilku wartości i wyświetl wyniki
  */
 
 public class UnitConverter {
+    public static final double KM_TO_MILES = 0.621371;
+
+    static double kilometersToMiles(double km) {
+        return km * KM_TO_MILES;
+    }
+
+    static double milesToKilometers(double miles) {
+        return miles / KM_TO_MILES;
+    }
+
+    private UnitConverter() {}
+
+    public static void main(String[] args) {
+        System.out.println(UnitConverter.kilometersToMiles(20));
+        System.out.println(UnitConverter.milesToKilometers(20));
+    }
 }
