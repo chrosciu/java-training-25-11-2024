1a8518b6503d43fe83c1aab776cba5ca89e44fd4
Marcin Chrost <mchrost82@gmail.com>
2024-11-28 10:14:22 +0100

    UnitConverter & Config - exercises

---
+++ src/eu/chrost/javatraining/day4/exercises/statics/Config.java
@@ -0,0 +1,21 @@
+package eu.chrost.javatraining.day4.exercises.statics;
+
+/*
+
+Zadanie 5: Zarządzanie konfiguracją
+1.	Utwórz klasę Config zgodnie ze wzorcem singleton:
+o	Prywatne statyczne pole Config instance.
+o	Prywatny konstruktor.
+o	Publiczną metodę static Config getInstance().
+o	Stałe static final String APP_NAME = "MyApp", static final String VERSION = "1.0.0".
+o	Publiczne metody:
+	String getAppName() – zwraca nazwę aplikacji.
+	String getVersion() – zwraca wersję aplikacji.
+2.	W metodzie main():
+o	Pobierz instancję konfiguracji i wyświetl nazwę oraz wersję aplikacji
+
+
+ */
+
+public class Config {
+}
---
+++ src/eu/chrost/javatraining/day4/exercises/statics/UnitConverter.java
@@ -0,0 +1,16 @@
+package eu.chrost.javatraining.day4.exercises.statics;
+
+/*
+
+Zadanie 4: Konwersje jednostek
+1.	Utwórz klasę UnitConverter z polami:
+o	Stałe static final double KM_TO_MILES = 0.621371.
+2.	Dodaj statyczne metody:
+o	double kilometersToMiles(double km) – konwertuje kilometry na mile.
+o	double milesToKilometers(double miles) – konwertuje mile na kilometry.
+3.	W metodzie main():
+o	Wykorzystaj metody do przeliczania kilku wartości i wyświetl wyniki
+ */
+
+public class UnitConverter {
+}
