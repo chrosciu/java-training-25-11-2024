924ed58bb0eb6b7bdbd0917969c7b4972664f9f9
Marcin Chrost <mchrost82@gmail.com>
2024-11-28 09:55:48 +0100

    Counter exercise

---
+++ src/eu/chrost/javatraining/day4/exercises/statics/Counter.java
@@ -0,0 +1,21 @@
+package eu.chrost.javatraining.day4.exercises.statics;
+
+/*
+
+Zadanie 1: Licznik obiektów
+1.	Utwórz klasę Counter z polami:
+o	Prywatne statyczne pole int objectCount, które przechowuje liczbę utworzonych obiektów klasy.
+o	Prywatne pole int id, przechowujące unikalny identyfikator obiektu.
+2.	Dodaj:
+o	Konstruktor, który zwiększa wartość objectCount i przypisuje jej wartość do pola id.
+o	Metodę static int getObjectCount(), która zwraca liczbę utworzonych obiektów.
+o	Metodę String toString() zwracającą tekstową reprezentację obiektu.
+3.	W metodzie main():
+o	Utwórz kilka obiektów klasy Counter.
+o	Wyświetl ich id oraz liczbę utworzonych obiektów za pomocą getObjectCount().
+
+
+ */
+
+public class Counter {
+}
