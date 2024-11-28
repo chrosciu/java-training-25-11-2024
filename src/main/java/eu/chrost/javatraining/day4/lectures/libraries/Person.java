1c1952117ca8329e7e0dd78545408159c1d38c7f
Marcin Chrost <mchrost82@gmail.com>
2024-11-28 14:02:38 +0100

    Added Gson demo (PersonSerializationDemo)

---
+++ src/main/java/eu/chrost/javatraining/day4/lectures/libraries/Person.java
@@ -0,0 +1,22 @@
+package eu.chrost.javatraining.day4.lectures.libraries;
+
+public class Person {
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
