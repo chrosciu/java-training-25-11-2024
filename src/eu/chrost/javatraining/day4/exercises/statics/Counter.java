ba6ec7abf27b1d8a95f2a26fe3911c4b47725b31
Marcin Chrost <mchrost82@gmail.com>
2024-11-28 10:10:04 +0100

    Counter - solution

--- src/eu/chrost/javatraining/day4/exercises/statics/Counter.java
+++ src/eu/chrost/javatraining/day4/exercises/statics/Counter.java
@@ -18,4 +18,36 @@ o	Wyświetl ich id oraz liczbę utworzonych obiektów za pomocą getObjectCount(
  */
 
 public class Counter {
+    private static int objectCount;
+    private final int id;
+
+    public Counter() {
+        ++objectCount;
+        this.id = objectCount;
+    }
+
+    public static int getObjectCount() {
+        return objectCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Counter{" +
+                "id=" + id +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        System.out.println(getObjectCount());
+
+        Counter c3 = new Counter();
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        System.out.println(Counter.getObjectCount());
+    }
 }
