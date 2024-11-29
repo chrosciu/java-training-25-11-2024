2c7e864dd2605b4b01379a2b472e94761b7f9c3c
Marcin Chrost <mchrost82@gmail.com>
2024-11-29 15:41:16 +0100

    Get region by id, Optional, method reference

--- src/main/java/eu/chrost/javatraining/day5/lectures/jdbc/RegionsDao.java
+++ src/main/java/eu/chrost/javatraining/day5/lectures/jdbc/RegionsDao.java
@@ -1,11 +1,13 @@
 package eu.chrost.javatraining.day5.lectures.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class RegionsDao {
     public List<Region> getAllRegions() {
@@ -42,5 +44,41 @@ public class RegionsDao {
             }
         }
         return regions;
+    }
+
+    public Optional<Region> getRegionById(long id) {
+        Region region = null;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DatabaseConnector.getConnection();
+            statement = connection.prepareStatement("select * from regions where region_id = ?");
+            statement.setLong(1, id);
+            resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Long regionId = resultSet.getLong("region_id");
+                String regionName = resultSet.getString("region_name");
+                region = new Region(regionId, regionName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return Optional.ofNullable(region);
+
     }
 }
